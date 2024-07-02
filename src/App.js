3134f2dc713e18666282f984d38e7df15b168bd9
GROS Christian <christian-gros@outlook.fr>
2024-07-02 14:24:40 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { useRef, useLayoutEffect, useEffect } from 'react';
+import React, { useRef, useLayoutEffect, useEffect } from "react";
 import Navbar from "./components/NavbarBlurred";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
 
 export default function App() {
-
-  return (
-      <Navbar />
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <div>
+      profile
+      <nav>
+        <Link to="localhost:3000">Home</Link>
+      </nav>
+      {/* <a href="localhost:3000">home</a> */}
+    </div>
+  );
+};
+
+export default Profile;
