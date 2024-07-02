2141b8aba18d0cdabb50e7c3ddbced730e93add5
Quentin <q.lami.sn@gmail.com>
2024-07-02 16:14:43 +0200

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useRef, useLayoutEffect, useEffect } from 'react';
 import Navbar from "./components/NavbarBlurred";
-
+import LigneQuete from './components/LigneQuete';
 
 export default function App() {
 
   return (
-      <Navbar />
+      <LigneQuete />
   );
 }
---
+++ src/components/LigneQuete.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const LigneQuete = () => {
+  return (
+    <div>
+        
+           <tr> Éteindre toutes les lumières inutiles pendant une heure</tr>
+            Réduire la consommation d'électricité de 10% ce mois-ci
+            Éteindre complètement les appareils électroniques en veille la nuit
+            Installer des prises intelligentes pour couper l'alimentation des appareils en veille
+            Utiliser des ampoules LED dans toutes les pièces de la maison
+            Configurer des timers sur les appareils pour limiter leur temps de fonctionnement
+            Participer à une journée sans climatisation en utilisant des méthodes naturelles pour réguler la température intérieure
+            Éteindre les appareils de chauffage ou de refroidissement lorsqu'ils ne sont pas nécessaires
+            Utiliser des rideaux épais pour isoler les fenêtres en hiver et réduire la déperdition de chaleur
+            Planifier et optimiser les activités de lessive pour minimiser l'utilisation de l'eau chaude
+            Installer un système de chauffage/cooling programmable pour ajuster automatiquement la température selon les besoins
+            Réduire la consommation d'eau de 10% ce mois-ci en utilisant des techniques d'économie d'eau
+            Participer à un programme de sensibilisation sur la conservation de l'énergie dans la communauté
+            Calculer et suivre la consommation d'énergie mensuelle pour identifier les opportunités d'économie
+            Utiliser des thermostats intelligents pour optimiser le chauffage et le refroidissement de la maison
+            Réduire la consommation d'électricité en utilisant des appareils énergétiquement efficaces
+            Installer des détecteurs de mouvement pour l'éclairage extérieur afin de limiter l'utilisation de l'électricité
+            Participer à une journée sans voiture en utilisant des transports en commun ou le covoiturage
+            Utiliser des rideaux isolants pour maintenir la fraîcheur de la maison en été
+            Réduire l'empreinte carbon
+            
+
+    </div>
+  );
+};
+
+export default LigneQuete;
