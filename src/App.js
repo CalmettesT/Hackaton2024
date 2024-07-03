942b463aa78d305d3751288efe8f0a77b2345c2f
CalmettesT <78858536+CalmettesT@users.noreply.github.com>
2024-07-03 15:41:46 +0200

    FIX : Quetes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useRef, useLayoutEffect, useEffect } from "react";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
-// import SingIn from "./pages/SignIn";
-// import SingUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import MdpOublie from "./pages/MdpOublie";
 import { createRoot } from "react-dom/client";
@@ -10,11 +8,9 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-// import Dashboard from "./pages/Dashboard";
-// import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-// import MdpOublie from "./pages/MdpOublie";
+import AchevementQuete from "./components/AchevementQuete"
 
 const router = createBrowserRouter([
   {
@@ -38,15 +34,15 @@ const router = createBrowserRouter([
     element: <MdpOublie />,
   },
   {
+    path: "/quete",
+    element: <AchevementQuete />,
+  },
+  {
     path: "/test",
     element: <Home />,
   },
 ]);
 
-import React, { useRef, useLayoutEffect, useEffect } from 'react';
-import Navbar from "./components/NavbarBlurred";
-import LigneQuete from './components/LigneQuete';
-import AchevementQuete from './components/AchevementQuete';
 
 export default function App() {
   return <RouterProvider router={router} />;
